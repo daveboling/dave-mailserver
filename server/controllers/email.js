63986ab287f1a3ee2b08094a5a6c612187b566a8
Dave Boling <kadowki@gmail.com>
2014-11-30 10:28:52 -0600

    Added mailgun support on dave api!

--- server/controllers/email.js
+++ server/controllers/email.js
@@ -1,5 +1,19 @@
 'use strict';
 
+
+var api_key = process.env.MAIL_KEY;
+var domain = process.env.MAIL_DOMAIN;
+var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+
 exports.send = function (request, reply){
-    reply('we got it').code(200);
+  var data = {
+    from: request.payload.email,
+    to: 'kadowki@gmail.com',
+    subject: 'Portfolio Inquiry:' + request.payload.name,
+    text: 'Phone Number: ' + request.payload.phone + '\n' + request.payload.body
+  };
+
+  mailgun.messages().send(data, function (error, body){
+    reply(body);
+  });
 };
